af5569460863a1ae748da54978f1625351c45a6c
iyatan <iyatan.atchoro@mail.mcgill.ca>
2019-05-17 23:59:08 -0400

    exta code

--- main.js
+++ main.js
@@ -1,32 +1,48 @@
+//When inStock is false, bind a class to the “Out of Stock” p tag that adds  text-decoration: line-through to that element.
+
 var app = new Vue({
   el: "#app",
   data: {
+    brand: "Vue Mastery",
     product: "Socks",
-    image: "./assets/vmSocks-green-onWhite.jpg",
-    inStock: "false",
-    details: ["80% coton", "20% polyster", "Gender-neutral"],
+    selectedVariant: 0,
+    details: ["80% cotton", "20% polyester", "Gender-neutral"],
     variants: [
       {
         variantId: 2234,
         variantColor: "green",
         variantImage:
-          "https://www.vuemastery.com/images/challenges/vmSocks-green-onWhite.jpg"
+          "https://www.vuemastery.com/images/challenges/vmSocks-green-onWhite.jpg",
+        variantQuantity: 10
       },
       {
         variantId: 2235,
         variantColor: "blue",
         variantImage:
-          "https://www.vuemastery.com/images/challenges/vmSocks-blue-onWhite.jpg"
+          "https://www.vuemastery.com/images/challenges/vmSocks-blue-onWhite.jpg",
+        variantQuantity: 0
       }
     ],
     cart: 0
   },
   methods: {
-    addToCard: function() {
+    addToCart() {
       this.cart += 1;
     },
-    updateProduct(variantImage) {
-      this.image = variantImage;
+    updateProduct(index) {
+      this.selectedVariant = index;
+      console.log(index);
+    }
+  },
+  computed: {
+    title() {
+      return this.brand + " " + this.product;
+    },
+    image() {
+      return this.variants[this.selectedVariant].variantImage;
+    },
+    inStock() {
+      return this.variants[this.selectedVariant].variantQuantity;
     }
   }
 });
